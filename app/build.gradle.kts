a34ecf7ee9cf0f519398dfa14eefbb582c73218a
Serpopard <ndiramio@gmail.com>
2026-01-19 15:14:33 -0500

    Fuck my stupid fucking life and mobile app dev this is my 13th reason I stg

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,5 +83,5 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
-
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 }
